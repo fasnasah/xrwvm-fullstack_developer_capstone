d439fea60a3eca4da2e71810ad34171de8413961
fasnasah <147029113+fasnasah@users.noreply.github.com>
2026-04-05 12:42:55 +0530

    Update Register.jsx

--- server/frontend/src/components/Register/Register.jsx
+++ server/frontend/src/components/Register/Register.jsx
@@ -4,12 +4,14 @@ function Register() {
   return (
     <div>
       <h2>Sign-up</h2>
-      <input type="text" placeholder="Username" />
-      <input type="text" placeholder="First Name" />
-      <input type="text" placeholder="Last Name" />
-      <input type="email" placeholder="Email" />
-      <input type="password" placeholder="Password" />
-      <button>Register</button>
+      <form>
+        <input type="text" placeholder="Username" />
+        <input type="text" placeholder="First Name" />
+        <input type="text" placeholder="Last Name" />
+        <input type="email" placeholder="Email" />
+        <input type="password" placeholder="Password" />
+        <button type="submit">Register</button>
+      </form>
     </div>
   );
 }
